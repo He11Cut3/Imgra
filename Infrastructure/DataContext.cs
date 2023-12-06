4f1bde387f778a0d991ee12daef8fd5c25219487
Hells <96310786+He11Cut3@users.noreply.github.com>
2023-12-06 14:15:44 +0300

    v1

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using ImgR.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ImgR.Infrastructure
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        public DbSet<Order> Orders { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Определение отношения между пользователями и заказами
+            builder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
+        }
+    }
+}
